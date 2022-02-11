9e0c41f0c10bfe3b7daeb97bc0b9ff047243866b
carloszrcom <carlos.z.r.78@gmail.com>
2022-02-11 11:42:18 +0100

    Validación de imagen antes de subirla

--- angular/clientes-app/src/app/clientes/detalle/detalle.component.ts
+++ angular/clientes-app/src/app/clientes/detalle/detalle.component.ts
@@ -14,7 +14,7 @@ export class DetalleComponent implements OnInit {
 
   cliente: Cliente;
   titulo: string = "Detalle del cliente";
-  private fotoSeleccionada: File;
+  public fotoSeleccionada: File;
 
   // Inyectamos clienteService vía constructor.
   // Inyectamos también ActivatedRoute para poder suscribir cuando cambia el parámetro del id.
@@ -53,14 +53,30 @@ export class DetalleComponent implements OnInit {
   public seleccionarFoto(event) {
     this.fotoSeleccionada = event.target.files[0];
     console.log(this.fotoSeleccionada);
+
+    // Comprobar que la foto es un archivo de imagen.
+    if (this.fotoSeleccionada.type.indexOf('image') < 0) {
+      Swal.fire('Error', 'Debe seleccionar un archivo de imagen válido', 'error');
+      this.fotoSeleccionada = null;
+    }
   }
 
+  /**
+   * Subir foto, botón Aceptar.
+   */
+
   public subirFoto() {
-    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(
-      cliente => {
-        this.cliente = cliente;
-        Swal.fire('La foto se ha subido completamente', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
-      }
-    );
+
+    if (!this.fotoSeleccionada) {
+      Swal.fire('Error al subir foto', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(
+        cliente => {
+          this.cliente = cliente;
+          Swal.fire('La foto se ha subido completamente', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
+        }
+      );
+    }
+
   }
 }
