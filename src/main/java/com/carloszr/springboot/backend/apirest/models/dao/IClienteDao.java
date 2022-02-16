9ae36926ee93e5b9dd19fd7edd205a0acc8f27bc
carloszrcom <carlos.z.r.78@gmail.com>
2022-02-16 10:58:39 +0100

    Creado atributo Region en Cliente y método para listar regiones en IClienteDao.

--- src/main/java/com/carloszr/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/carloszr/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,10 +1,23 @@
 package com.carloszr.springboot.backend.apirest.models.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.carloszr.springboot.backend.apirest.models.entity.Cliente;
+import com.carloszr.springboot.backend.apirest.models.entity.Region;
+
+// No extiende de CrudRepository para poder implementar paginación.
 
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
+	// Tenemos que mapear el método a una consulta JPQL(JPA Query), que es la forma de 
+	// utilizar repositories en Spring Data JPA.
+	// Recordar que en la Query estamos trabajando con OBJETOS y no con tablas.
+	// "from Region", una consulta abreviada para que retorne todas las regiones.
+	
+	@Query("from Region")
+	public List<Region> findAllRegiones();
 }
--- src/main/java/com/carloszr/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/carloszr/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 //import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -16,6 +18,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "clientes")
@@ -50,6 +54,24 @@ public class Cliente implements Serializable {
 	
 	@Column(name = "foto")
 	private String foto;
+	
+	// Un cliente puede tener una región, pero una región puede tener muchos clientes.
+	// Entonces, relación ManyToOne.
+	
+	// LAZY, carga perezosa, cuando se llame al get entonces se realiza la carga.
+	// Al trabajar con API REST, el listado de clientes
+	// se va a transformar en un JSON de forma automática,
+	// En el caso de Región se genera otro objeto, otro JSON, anidado dentro del
+	// JSON principal de Clientes. Cuando se invoca getRegion va a realizar la consulta a la bd,
+	// para acceder a estos objetos relacionados. Debajo, de forma transparente va a generar un objeto
+	// Region  que es un proxy, es como un puente para poder acceder a estos datos.
+	// Si omitimos la anotación JoinColumn, va a generar de todas formas un campo, pero mejor no omitirlo.
+	// El proxy genera atributos adicionales. Mejor ignorarlos para que no de error.
+	
+	@ManyToOne
+	@JoinColumn(name = "region_id")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Region region;
 	
 	/**
 	 * Evento del ciclo de vida de las clases entity.
@@ -108,6 +130,14 @@ public class Cliente implements Serializable {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
 	}
 
 	/**
